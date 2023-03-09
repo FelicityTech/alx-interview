28e544c38d5cec110751851fbdd49467498aa8a6
Solomon Eniola Adegoke <souceking@gmail.com>
2023-03-09 14:12:42 -0800

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,23 +1,22 @@
-#!/usr/bin/node
-const argv = process.argv;
+t argv = process.argv;
 const request = require('request');
 
 function getPromise (url) {
-  const promise = new Promise(function (resolve, reject) {
-    request(url, function (err, res, body) {
-      if (err) reject(err);
-      else resolve(JSON.parse(body).name);
-    });
-  });
-  return promise;
+	  const promise = new Promise(function (resolve, reject) {
+		      request(url, function (err, res, body) {
+			            if (err) reject(err);
+			            else resolve(JSON.parse(body).name);
+			          });
+		    });
+	  return promise;
 }
 
 request(`https://swapi-api.hbtn.io/api/films/${argv[2]}`, async function (err, res, body) {
-  if (err) throw err;
-  const data = JSON.parse(body).characters;
+	  if (err) throw err;
+	  const data = JSON.parse(body).characters;
 
-  for (let i = 0; i < data.length; i++) {
-    const result = await getPromise(data[i]);
-    console.log(result);
-  }
+	  for (let i = 0; i < data.length; i++) {
+		      const result = await getPromise(data[i]);
+		      console.log(result);
+		    }
 });
